568802b7ca8f33442aca18dffe6155b35845191d
yuliannabn <yulianna.barahona-naranjo@epitech.eu>
2024-05-15 10:01:41 +0200

    [del] bar_slots_by_blob_count: unused imports

--- plotly_gen/plots/bar/slots_by_blob_count.py
+++ plotly_gen/plots/bar/slots_by_blob_count.py
@@ -1,7 +1,7 @@
 from plots.bar.bar import bar_create_fig
 from creates import df_clickhouse_create
 from sessions import BLOB_SIDECAR_TABLE
-from utils import bold, fill_in_gaps, title_format, get_epoch_readable_unit
+from utils import bold, fill_in_gaps, get_epoch_readable_unit
 
 
 def bar_slots_by_blob_count_create(client):
