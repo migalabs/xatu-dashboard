e9d9e173e3c643c8f549b935863d74567f1830d2
yuliannabn <yulianna.barahona-naranjo@epitech.eu>
2024-05-28 20:11:14 +0200

    [mod] builder_functions update

--- plotly_gen/builder_functions.py
+++ plotly_gen/builder_functions.py
@@ -6,16 +6,18 @@ import plots.box as box
 import plots.pie as pie
 
 blob_functions = [
-    current_timestamp_get,                   # 1
-    bar.blobs_per_slot_create,               # 2
-    bar.slots_by_blob_count_create,          # 3
-    pie.slots_by_blob_count_create,          # 4
-    bar.first_last_difference_create,        # 5
-    box.first_last_difference_create,        # 6
-    area.blob_hash_repetitions_create,       # 7
-    area.average_blob_arrival_create,        # 8
-    area.blob_size_used_per_blob_create,     # 9
-    box.block_size_vs_blobs_create,          # 10
+    current_timestamp_get,                              # 1
+    bar.blobs_per_slot_create,                          # 2
+    bar.slots_by_blob_count_create,                     # 3
+    pie.slots_by_blob_count_create,                     # 4
+    bar.first_last_difference_create,                   # 5
+    box.first_last_difference_create,                   # 6
+    area.blob_hash_repetitions_create,                  # 7
+    area.average_blob_arrival_create,                   # 8
+    area.blob_size_used_per_blob_create,                # 9
+    box.block_size_vs_blobs_create,                     # 10
+    area.type_3_transactions_per_block_create,          # 11
+    area.day_type_3_transactions_per_block_create       # 12
 ]
 
 builder_functions = blob_functions
@@ -32,15 +34,10 @@ def get_test_functions(args, functions=builder_functions) -> List[Callable]:
 
     if (n_args > 0):
         testing_list = []
-
         for index in range(0, n_args):
             function_index = int(args[index]) - 1
-
             if (index == n_args or function_index >= len(testing_functions)):
                 break
-
             testing_list.append(testing_functions[function_index])
-
         return (testing_list)
-
     return (testing_functions)
